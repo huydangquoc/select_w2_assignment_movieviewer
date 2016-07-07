05a0b79695d8494eb9d6e341b24a73e21d1c145d
Dang Quoc Huy <huysamdua@yahoo.com>
2016-07-07 12:47:08 +0700

    Setup Search View

---
+++ MovieViewer/MoviesSearchViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MoviesSearchViewController.swift
+//  MovieViewer
+//
+//  Created by Dang Quoc Huy on 7/7/16.
+//  Copyright © 2016 Dang Quoc Huy. All rights reserved.
+//
+
+import UIKit
+
+class MoviesSearchViewController: UIViewController {
+
+    var searchBar = UISearchBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // set delegate
+        searchBar.delegate = self
+        
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// SearchBar methods
+extension MoviesSearchViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true;
+    }
+    
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
